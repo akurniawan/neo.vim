4fbe2b23dea678fc29e7750f87c7c91c343cd38c
Aditya Kurniawan <akurniaw@amazon.com>
2024-03-08 16:34:48 +0000

    update exit command

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -9,12 +9,7 @@ return {
             formatters_by_ft = {
                 javascript = { "prettier" },
                 typescript = { "prettier" },
-                javascriptreact = { "prettier" },
                 typescriptreact = { "prettier" },
-                svelte = { "prettier" },
-                css = { "prettier" },
-                html = { "prettier" },
-                json = { "prettier" },
                 yaml = { "prettier" },
                 markdown = { "prettier" },
                 graphql = { "prettier" },
